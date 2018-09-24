9108faa24ee70b99895db479bb760d1912cf6228
hxjia96 <43189295+hxjia96@users.noreply.github.com>
2018-09-23 23:18:41 -0400

    made comments to the program

--- tweets_video.py
+++ tweets_video.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 # encoding: utf-8
-#Author - Prateek Mehta
+# Copyright 2018 Haoxuan Jia hxjia@bu.edu
 
 
 import tweepy #https://github.com/tweepy/tweepy
 import json
 import urllib.request
 import os
+import io 
 import PIL
-import io
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -16,43 +16,39 @@ from PIL import ImageFont
 from google.cloud import vision
 from google.cloud.vision import types
 
+# global list to hold the url got from tweepy and description got from clould vision
 picpaths = []
 piclabel = []
 
-
 #Twitter API credentials
-
 global consumer_key
 global consumer_secret
 global access_key
 global access_secret
-
-#def vision_path(visionpath):
-#    os.system('export GOOGLE_APPLICATION_CREDENTIALS=' + '"' + str(visionpath) + '"')
     
 
-def get_all_tweets(screen_name):
-    
-    #Twitter only allows access to a users most recent 3240 tweets with this method
-    
+def get_all_tweets(screen_name):    
     #authorize twitter, initialize tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_key, access_secret)
     api = tweepy.API(auth)
     
+    #create a fold to keep the images
     if not os.path.exists('pic'):
         os.mkdir('pic')
-    #initialize a list to hold all the tweepy Tweets
-#    os.mkdir('pic')
+    
+    #a list to keep all the tweet information
     alltweets = []
-#    mediaurls = []
+
     n = 1
+    
     #make initial request for most recent tweets (200 is the maximum allowed count)
     new_tweets = api.user_timeline(screen_name = screen_name,count=200)
     
     #save most recent tweets
     alltweets.extend(new_tweets)
     
+    #get the url of images and save them as image1.jpg, image2.jpg, ...
     for tweet in alltweets:
         entities = tweet._json['entities']
         if 'media' not in entities:
@@ -60,33 +56,31 @@ def get_all_tweets(screen_name):
         else:
             media = entities['media']
             mediaurl = media[0]['media_url']
-#            mediaurls.append(mediaurl)
             picpath = 'pic/image' + str(n) + '.jpg'
             picpaths.append(picpath)
             urllib.request.urlretrieve(mediaurl, picpath)
             n = n + 1
 
 def make_video():
+    # use FFMPEG to create a video out of images in pic folder
     os.system("ffmpeg -framerate 24 -r 1 -i /home/ece-student/pic/image%d.jpg -s 1080*1080 output.mp4")        
 
 def get_label():
+    #get labels information of the images from google cloud vision
     client = vision.ImageAnnotatorClient()
     for imagepath in picpaths:
         file_name = os.path.join(
             os.path.dirname(__file__),
             imagepath)
-
         with io.open(file_name, 'rb') as image_file:
             content = image_file.read()
-
         image = types.Image(content=content)
-
         response = client.label_detection(image=image)
         labels = response.label_annotations
-        
+
+        # get the description from the label information and put them three in a row
         descriptions = []
         description= ''        
-
         num = 0
         row = 0
         for label in labels:
@@ -96,35 +90,25 @@ def get_label():
                 descriptions.append(description)
                 description = ''
                 row = row + 1
-
-#        print(num)   
-# add label to image        
-
         
+        #add description to each of the images
         imageFile = imagepath
-        im = Image.open(imageFile)
-        
-        font = ImageFont.truetype('LiberationSans-Regular.ttf', 24)        
-    
+        im = Image.open(imageFile)        
+        font = ImageFont.truetype('LiberationSans-Regular.ttf', 24)            
         draw = ImageDraw.Draw(im)
         for i in range(row):            
             draw.text((0, 30*i), descriptions[i], (255, 0, 0),font = font)    
         draw = ImageDraw.Draw(im)                          
-
         im.save(imagepath)
 
-
+# the whole process of making a video from images from tweets
 def get_video(screen_name):
     get_all_tweets(screen_name)
     get_label()
     make_video()
     
     
-
-
+#runing directly from this file    
 if __name__ == '__main__':
-    #pass in the username of the account you want to download
-    get_all_tweets("@Ibra_official")
-#    get_all_tweets("@BU_Tweets")
-    get_label()
-    make_video()
+    get_video("@Ibra_official")
+
