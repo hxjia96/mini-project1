5b60b2b063daca3b00f24cf835f8a746e97ec4c2
hxjia96 <43189295+hxjia96@users.noreply.github.com>
2018-09-24 00:13:33 -0400

    make it able to run in terminal

--- run.py
+++ run.py
@@ -1,9 +1,16 @@
-import tweets_video
 #run 'export GOOGLE_APPLICATION_CREDENTIALS="your cloud vison json file"' in terminal
+import tweets_video
 
-tweets_video.consumer_key = "Enter the consumer key"
-tweets_video.consumer_secret = "Enter the consumer secret"
-tweets_video.access_key = "Enter the access key"
-tweets_video.access_secret = "Enter the access secret"
+consumer_key = input("Enter your consumer key:")
+consumer_secret = input("Enter the consumer secret")
+access_key = input("Enter the access key")
+access_secret = input("Enter the access secret")
+screen_name = input("Enter the screen name:")
 
-tweets_video.get_video("Enter the screen name")
+tweets_video.consumer_key = consumer_key
+tweets_video.consumer_secret = consumer_secret
+tweets_video.access_key = access_key
+tweets_video.access_secret = access_secret
+
+
+tweets_video.get_video(screen_name)
